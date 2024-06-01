6fa9a47f23e476a53ffe25a45baa972b7c4a0c82
kennylewi <kennylewi27@gmail.com>
2024-06-01 12:26:29 +0700

    test

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
@@ -10,6 +9,14 @@ app.use("/auth", require("./routes/jwtAuth"));
 app.use("/dashboard", require("./routes/dashboard"));
 app.use("/admin", require("./routes/admin"));
 app.use("/calendar", require("./routes/calendar"));
+
+app.use("/", (req, res) => {
+  try {
+    res.json("Hello World");
+  } catch (err) {
+    console.error(err.message);
+  }
+})
 
 app.listen(5000, () => {
   console.log("Server started on port 5000");
