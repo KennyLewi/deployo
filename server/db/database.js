101be3f65ee07a1f2af32eec3c7826cf090d5be0
kennylewi <kennylewi27@gmail.com>
2024-06-01 13:36:12 +0700

    testnew

--- server/db/database.js
+++ server/db/database.js
@@ -1,11 +1,17 @@
 const Pool = require("pg").Pool;
+require('dotenv').config();
 
 const pool = new Pool({
-  user: "postgres",
-  password: "postgres",
-  host: "localhost",
-  port: 5432,
-  database: "optiplan",
+  // user: "postgres",
+  // password: "postgres",
+  // host: "localhost",
+  // port: 5432,
+  // database: "optiplan",
+  user: "postgres.ywesugobnkedylbyfppg",
+  password: process.env.DB_P,
+  host: "aws-0-ap-southeast-1.pooler.supabase.com",
+  port: 6543,
+  database: "postgres"
 });
 
 module.exports = pool;
--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+require("dotenv").config();
+
+console.log(process.env.DB_P);
 
 app.use(cors());
 app.use(express.json());
